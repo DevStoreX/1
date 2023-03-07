3307b7f2f2533084c661744904e14b2d93783f31
DevStoreX <jhemmo777@gmail.com>
2023-03-07 01:47:13 -0500

    .

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ const navbarMenu = document.querySelector(".navbar-nav");
 navbarToggler.addEventListener("click", function () {
   navbarMenu.classList.toggle("collapsed");
 });
+
+/* Script for transparent nav bar */
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 50) {
+    $('.navbar').addClass('navbar-transparent');
+  } else {
+    $('.navbar').removeClass('navbar-transparent');
+  }
+});
+
+
+const bgImages = [
+  'https://source.unsplash.com/1600x900/?nature',
+  'https://source.unsplash.com/1600x900/?water',
+  'https://source.unsplash.com/1600x900/?mountain',
+  'https://source.unsplash.com/1600x900/?city'
+];
+
+let bgIndex = 0;
+const bgImage = document.querySelector('#home');
+
+function changeBgImage(index) {
+  bgImage.style.backgroundImage = `url(${bgImages[index]})`;
+}
+
+document.querySelector('#left-arrow').addEventListener('click', () => {
+  bgIndex = bgIndex === 0 ? bgImages.length - 1 : bgIndex - 1;
+  changeBgImage(bgIndex);
+});
+
+document.querySelector('#right-arrow').addEventListener('click', () => {
+  bgIndex = bgIndex === bgImages.length - 1 ? 0 : bgIndex + 1;
+  changeBgImage(bgIndex);
+});
